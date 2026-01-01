ca15dfbf5140c801e1a3787c53bba83f05cda860
Serlan-code <alanyaserfadlurrahman@gmail.com>
2026-01-02 00:15:27 +0700

    Add files via upload

---
+++ projekphp/index.php
@@ -0,0 +1,31 @@
+<?php
+echo "<h1>PRODI SISTEM INFORMASI 2025</h1>";
+echo "<hr>";
+
+echo "<p><b>Institut Teknologi dan Ilmu Sosial Khatulistiwa</b></p>";
+
+echo "<a href='index.php'>Beranda</a> | ";
+echo "<a href='profilkampus.php'>Profil Kampus</a> | ";
+echo "<a href='informasipendaftaran.php'>Informasi Pendaftaran</a> | ";
+echo "<a href='kontak.php'>Kontak</a>";
+
+echo "<hr>";
+
+echo "<h2>Selamat Datang</h2>";
+
+echo "<p>
+Selamat datang di Sistem Informasi Pendaftaran Mahasiswa Baru.
+Website ini digunakan untuk membantu calon mahasiswa melakukan
+pendaftaran secara online dengan mudah dan cepat.
+</p>";
+
+echo "<p>
+Silakan klik tombol di bawah ini untuk melakukan pendaftaran.
+</p>";
+
+echo "<a href='tambah.php'>DAFTAR SEKARANG</a>";
+
+echo "<hr>";
+
+echo "<p>© 2025 Program Studi Sistem Informasi</p>";
+?>
---
+++ projekphp/kontak.php
@@ -0,0 +1,6 @@
+<?php
+ echo "<h5>Nama: AY.Fadlurrahman</h5>";
+ echo "<h5>Tanggal lahir: 05-07-2007</h5>";
+ echo "<h5>Prodi: Sistem Informasi</h5>";
+ Echo "<h5>Nomor Telepon: 085321868174</h5>";
+ ?>
